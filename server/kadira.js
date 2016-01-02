54dd650807e5e779de3656d7965b1db191b7f74c
Steven Scaffidi <sscaff1@gmail.com>
2016-01-02 10:54:53 -0800

    added kadira - commenting it out until I take down the old app

---
+++ server/kadira.js
@@ -0,0 +1,5 @@
+// Meteor.startup(function() {
+//   var appId = Meteor.settings.kadira.appId;
+//   var appSecret = Meteor.settings.kadira.appSecret;
+//   Kadira.connect(appId, appSecret);
+// });
