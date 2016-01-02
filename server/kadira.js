f34d3cf786222089d30897de6e9c09b8c19e85b6
Steven Scaffidi <sscaff1@gmail.com>
2016-01-02 15:24:11 -0800

    getting rid of kadira duplication

--- server/kadira.js
+++
@@ -1,5 +0,0 @@
-// Meteor.startup(function() {
-//   var appId = Meteor.settings.kadira.appId;
-//   var appSecret = Meteor.settings.kadira.appSecret;
-//   Kadira.connect(appId, appSecret);
-// });
